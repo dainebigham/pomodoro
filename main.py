fa7c3141deadba0908913e97f366fe9de3d20541
Daine Bigham <daine.bigham@gmail.com>
2021-06-15 16:55:40 +1200

    created app window and constants

---
+++ main.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+# constants
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK = 25
+SHORT_BREAK = 5
+LONG_BREAK = 20
+
+
+window = Tk()
+window.title('PyPomodoro')
+
+
+
+window.mainloop()
