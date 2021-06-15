16483ce1cc5780e67fa8f5deef90eebf88ad8ad1
Daine Bigham <daine.bigham@gmail.com>
2021-06-15 17:47:19 +1200

    completed layout for the UI

--- main.py
+++ main.py
@@ -17,7 +17,11 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 img = PhotoImage(file='tomato.png')
 canvas.create_image(102, 112, image=img)
-canvas.create_text(102, 130, text='00:00', fill='white', font=(FONT))
-canvas.pack()
+canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT))
+canvas.grid(row=2, column=2)
+
+button_start = Button(text='Start', bg='white', highlightthickness=0).grid(row=3, column=0, pady=10)
+button_reset = Button(text='Reset', bg='white', highlightthickness=0).grid(row=3, column=3, pady=10)
+checkmarks = Label(text='✓', fg=GREEN, bg=YELLOW).grid(row=3, column=2, pady=10)
 
 window.mainloop()
